e4391b1ed03a55257a7bc4b62543246779127bd0
Kavi-md <155098034+imeshkawshalya@users.noreply.github.com>
2025-01-15 10:20:31 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=f7JHDRzQ#q4IttDCdjAQcSrkjP1EeyQ539ON5ICOzH4z5-ec28ns' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=nqoxBbQR#irip-DDcu9kjxMDVK7UWgEB7aecE_Nw0HJA7lqUGG-8' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
